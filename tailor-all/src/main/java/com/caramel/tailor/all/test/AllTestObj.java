a4d6d9a1539062e8e2f90114372181ec7902b0cf
fitminf <fitminf@163.com>
2022-01-14 15:37:17 +0800

    test：测试模块调用

---
+++ tailor-all/src/main/java/com/caramel/tailor/all/test/AllTestObj.java
@@ -0,0 +1,26 @@
+package com.caramel.tailor.all.test;
+
+import com.caramel.tailor.core.test.TestObj;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * AllTestObj
+ *
+ * @author Fitminf
+ * @since 2022-01-13 01:09
+ */
+public class AllTestObj {
+
+    public void msg(){
+        TestObj obj = new TestObj();
+        obj.setId("123");
+        obj.setName("小明");
+        obj.msg();
+    }
+
+    public void utilTest(){
+        String name = "fitminf";
+
+        System.out.println(StringUtils.isNotBlank(name));
+    }
+}
