31cca4bdff117f4c4b27e85ed563a4240bab4ced
fitminf <fitminf@163.com>
2022-05-31 00:04:55 +0800

    feat: 添加订制参数异常

--- tailor-core/src/main/java/com/caramel/tailor/core/exception/TailorException.java
+++ tailor-core/src/main/java/com/caramel/tailor/core/exception/TailorException.java
@@ -2,13 +2,13 @@ package com.caramel.tailor.core.exception;
 
 /**
  * TailorException
- *
+ * <p>
  * 定制异常超类
  *
  * @author Fitminf
  * @since 2022-05-17 01:44
  */
-public class TailorException extends RuntimeException{
+public class TailorException extends RuntimeException {
 
     public TailorException(String message) {
         super(message);
---
+++ tailor-core/src/main/java/com/caramel/tailor/core/exception/TailorParameterException.java
@@ -0,0 +1,18 @@
+package com.caramel.tailor.core.exception;
+
+import com.caramel.tailor.core.util.TailorEmptyUtil;
+
+/**
+ * TailorParameterException
+ * <p>
+ * 订制参数异常
+ *
+ * @author Fitminf
+ * @since 2022-05-30 23:46
+ */
+public class TailorParameterException extends TailorException {
+
+    public TailorParameterException(String message) {
+        super(message);
+    }
+}
