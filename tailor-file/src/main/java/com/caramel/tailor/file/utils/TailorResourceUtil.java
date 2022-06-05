1dc880b384bc68d407f98a9799ec6f44269c98dd
fitminf <fitminf@163.com>
2022-06-05 14:48:50 +0800

    feat: 资源相关工具获取路径功能优化

--- tailor-file/src/main/java/com/caramel/tailor/file/utils/TailorResourceUtil.java
+++ tailor-file/src/main/java/com/caramel/tailor/file/utils/TailorResourceUtil.java
@@ -2,6 +2,8 @@ package com.caramel.tailor.file.utils;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.File;
+
 /**
  * TailorResourceUtil
  * <p>
@@ -15,23 +17,28 @@ public class TailorResourceUtil {
     /**
      * 获取项目资源路径
      *
+     * @param childrenProjectName 子工程名称
      * @return 路径
      */
-    public static String classpath() {
-        return System.getProperty("user.dir") + "/src/main/resources";
+    public static String classpath(String childrenProjectName) {
+        return System.getProperty("user.dir") + "/" + childrenProjectName + "/src/main/resources";
     }
 
     /**
      * 获取项目资源路径
      *
-     * @param path resources 后面的路径
+     * @param childrenProjectName 子工程名称
+     * @param path                resources 后面的路径
      * @return 路径
      */
-    public static String classpath(String path) {
+    public static String classpath(String childrenProjectName, String path) {
         if (StringUtils.isNotEmpty(path) && !path.startsWith("/")) {
             path = "/" + path;
         }
-        return System.getProperty("user.dir") + "/src/main/resources" + path;
+        return new File("").getAbsolutePath() + "/" + childrenProjectName + "/src/main/resources" + path;
     }
 
+    public static void main(String[] args) {
+        System.out.println(classpath("tailor-file"));
+    }
 }
--- tailor-file/src/test/java/test/com/caramel/tailor/file/UtilsTest.java
+++ tailor-file/src/test/java/test/com/caramel/tailor/file/UtilsTest.java
@@ -5,6 +5,7 @@ import com.caramel.tailor.file.utils.TailorResourceUtil;
 import com.itextpdf.text.DocumentException;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -32,7 +33,22 @@ public class UtilsTest {
      * 测试
      */
     @Test
-    public void testTailorResourceUtil(){
-        System.out.println(TailorResourceUtil.classpath("asd/sss"));
+    public void testTailorResourceUtil() throws IOException {
+        // 1.获取当前文件所在的路径
+        System.out.println(this.getClass().getResource("").getPath());
+        // 2.获取再 target 下 classpath 路径
+        System.out.println(this.getClass().getResource("/").getPath());
+        // 3.也是获取 classpath 的绝对路径
+        System.out.println(Thread.currentThread().getContextClassLoader().getResource("").getPath());
+        // 4.也是获取 classpath 的绝对路径
+        System.out.println(this.getClass().getClassLoader().getResource("").getPath());
+        // 5.也是获取 classpath 的绝对路径
+        System.out.println(ClassLoader.getSystemResource("").getPath());
+        // 6.获取当前项目路径（此方法与 7 效果相同，但是可以将路径转为标准形式，会处理“.”和“..”）
+        System.out.println(new File("").getCanonicalPath());
+        // 7.获取项目绝对路径（不会处理“.”和“..”）
+        System.out.println(new File("").getAbsolutePath());
+
+        System.out.println("-----------"+TailorResourceUtil.classpath("tailor-file"));
     }
 }
